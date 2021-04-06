3804a78c5b3ff3be8e6148e56682d13c8ab3644d
Alx M <7true@mail.ru>
2021-04-06 23:58:24 +0700

    HW08 without Optional

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,39 @@
+const userAjaxUrl = "profile/meals/";
+
+const ctx = {
+    ajaxUrl: userAjaxUrl
+};
+
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ]
+        })
+    );
+});
